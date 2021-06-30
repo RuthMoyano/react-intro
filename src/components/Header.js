36c036b6a3e87f723b86d590b511caf4fb2fc428
RuthMoyano <77814331+RuthMoyano@users.noreply.github.com>
2021-06-30 09:40:40 -0500

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import "../App.css"
+
+export class Header extends Component {
+    render() {
+        return (
+            <div className="header-div">
+                <h1>Ruth Moyano</h1>
+                <h3>enthusiastic, Web-developer in the making, and problem-solver</h3>
+            </div>
+        )
+    }
+}
+
+export default Header
