09435be5349dfd7bad403c685b99eb86e3d46413
RuthMoyano <77814331+RuthMoyano@users.noreply.github.com>
2021-06-30 09:37:32 -0500

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "../App.css"
+
+export class Footer extends Component {
+    render() {
+        return (
+            <div className="footer-div">
+                © Ruth Angela
+            </div>
+        )
+    }
+}
+
+export default Footer
